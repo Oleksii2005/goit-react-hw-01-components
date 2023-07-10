95241cb10e9d50a42c0a901c3e6d3442931c1ba3
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-07-10 22:48:06 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import css from './Statistics.module.css';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
@@ -16,15 +16,15 @@ export const Statistics = ({ title, stats }) => {
     </section>
   );
 };
-Statistics.propTypes = {
-  products: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      label: PropTypes.string.isRequired,
-      percentage: PropTypes.number.isRequired,
-    })
-  ),
-};
+// Statistics.propTypes = {
+//   products: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       label: PropTypes.string.isRequired,
+//       percentage: PropTypes.number.isRequired,
+//     })
+//   ),
+// };
 // export default Statistics;
 
 // import data from '../../data.json';
